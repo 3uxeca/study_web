ced83712820e9186339eb1b489658a52c7778eae
3uxeca <sajihye4@gmail.com>
2019-11-15 13:09:45 +0900

    11/15

---
+++ programmers/ex.py
@@ -0,0 +1,34 @@
+# def water_melon(n):
+#     return "수박"*(n//2) + "수"*(n%2)
+
+
+# print(water_melon(3))
+# print(4//2)
+# print(4%2)
+
+# def solution(seoul):
+#     location_kim = seoul.index("Kim")
+#     return location_kim
+
+# seoul = ["Jane", "Kim"]
+# print("김서방은 " + str(solution(seoul)) + "에 있다")
+
+# def solution(seoul):
+#     seoul = ["Kim", "Park", "Seo"]
+#     location_kim = seoul.index("Kim")
+    
+#     answer = "김서방은 " + str(location_kim) + "에 있다"
+#     return answer
+
+# test = ["Kim", "Park", "Seo"]
+# print(solution(test))
+
+def solution(seoul):
+    seoul = ["Jane", "Kim"]
+    location_kim = seoul.index("Kim")
+    
+    answer = "김서방은 " + str(location_kim) + "에 있다"
+    return print(answer)
+
+seoul = ["Jane", "Kim"]
+solution(seoul)
